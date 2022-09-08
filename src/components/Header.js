20cf4502a78c1851e5e2d4c5257b5d945bd3afa0
Vinicius <viniciusborges949@gmail.com>
2022-09-08 00:07:08 -0300

    pilot

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,13 @@ export function Header() {
       <header className={styles.header}>
         <img className={styles.logo} src={logo} alt="logo"/>
         <span className={styles.title}>Leo Furlanetto</span>
-        <nav>
+        <nav className={styles.menu}>
+            
             
             <a className={styles.active}href="#">Home</a>
-            <a href="#">Músicas</a>
-            <a href="#">Projetos</a>
-            <a href="#">Social</a>
-            
+            <a className={styles.active}href="#">Músicas</a>
+            <a className={styles.active}href="#">Projetos</a>
+            <a className={styles.active}href="#">Social</a>
             
         </nav>
       </header>
